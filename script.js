9e0a8182fe5e48f7c87a98d64f5cd568b7ece4ae
Your Name <khansameer44801@gmail.com>
2024-02-16 10:18:53 +0530

    add javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const logbtn=document.querySelector(".log-btn");
+logbtn.addEventListener("click",function(){
+    alert("your password was invalid.");
+
+    logbtn.innerHTML="sorry again enter:"
+})
